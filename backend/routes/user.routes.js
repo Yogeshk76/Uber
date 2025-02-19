b322dd3c83cdee313d81e3741879c9c8841396e1
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-02-19 11:57:02 +0530

    updated

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');  // to validate data
+const userController = require('../controllers/user.controller');
 
 
 router.post('/register', [
@@ -9,6 +10,13 @@ router.post('/register', [
   body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
 ],
   userController.registerUser
+);
+
+router.post('/login', [
+  body('email').isEmail().withMessage('Invalid email'),
+  body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
+],
+  userController.loginUser
 );
 
 module.exports = router;
