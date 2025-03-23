a7f8f434a20ae7b4eed0afe46dc3067a4a53f2ba
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-03-23 14:01:49 +0530

    google map api access pending

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,19 @@ import "./index.css";
 import App from "./App.jsx";
 import {BrowserRouter} from "react-router-dom";
 import UserContext from "./context/UserContext.jsx";
+import SocketProvider from "./context/SocketContext.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+  <>
     <UserContext>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <SocketProvider>  
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
     </UserContext>
-  </StrictMode>
+  </>
+</StrictMode>
 );
